7bb6a153f61f92f5fbcd78c67c8ea49a2c65b9bc
Kenzo Aaron Dela Cruz <kenzo.delacruz03@gmail.com>
2023-09-26 21:59:12 +0800

    Done with the players mechanics

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,30 +1,9 @@
 
-
-class Person
-
-    attr_reader :name
-    attr_writer :turn
-
-    def initialize(name)
-        @name = name
-    end
-
-    def show_name
-        puts name
-    end
-
-end
-
-
-print "What's the first player's name?: "
-
-first_player_temp = gets.chomp
-
-first_player = Person.new("#{first_player_temp}")
-
+# Make an array with 9 values from 1-9
 board = Array.new(9)
 board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+# Make a board for the game
 board = 
 <<-Board
 #{board[0]} | #{board[1]} | #{board[2]}
@@ -34,3 +13,100 @@ board =
 #{board[6]} | #{board[7]} | #{board[8]}
 Board
 
+
+class Person
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+end
+
+class First < Person
+
+    attr_reader :piece
+    attr_accessor :score
+
+    def initialize(name)
+        super(name)
+        @piece = "X"
+        @score = 0
+    end
+
+    def show_name
+        puts "Hello, my name is " + name
+    end
+
+    def show_piece
+        puts piece
+    end
+
+    def add_score
+        self.score += 1
+    end
+
+    def show_score
+        puts score
+    end
+
+end
+
+class Second < Person
+    
+    attr_reader :piece
+    attr_accessor :score
+
+    def initialize(name)
+        super(name)
+        @piece = 'O'
+        @score = 0
+    end
+
+    def show_name
+        puts "Hello, my name is " + name
+    end
+
+    def show_piece
+        puts piece
+    end
+
+    def add_score
+        self.score += 1
+    end
+
+    def show_score
+        puts score
+    end
+
+end
+
+def game(first_player, second_player)
+
+    print first_player.show_score
+
+    first_player.add_score
+
+    print first_player.show_score
+
+    if first_player.score == 1
+        puts "#{first_player.name} won"
+    end
+
+end
+
+
+print "What's the first player's name?: "
+
+temp = gets.chomp
+
+first_player = First.new("#{temp}")
+
+print "What is the second player's name?: "
+
+temp = gets.chomp
+
+second_player = Second.new("#{temp}")
+
+game(first_player, second_player)
