165eeb8960f8597d1ff752fa11236fc30a19d7b8
Kenzo Aaron Dela Cruz <kenzo.delacruz03@gmail.com>
2023-09-26 15:42:44 +0800

    Added a board for the game

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -2,8 +2,35 @@
 
 class Person
 
-    def initialize()
+    attr_reader :name
+    attr_writer :turn
+
+    def initialize(name)
+        @name = name
     end
-    
+
+    def show_name
+        puts name
+    end
+
 end
+
+
+print "What's the first player's name?: "
+
+first_player_temp = gets.chomp
+
+first_player = Person.new("#{first_player_temp}")
+
+board = Array.new(9)
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+board = 
+<<-Board
+#{board[0]} | #{board[1]} | #{board[2]}
+--|---|--
+#{board[3]} | #{board[4]} | #{board[5]}
+--|---|--
+#{board[6]} | #{board[7]} | #{board[8]}
+Board
 
